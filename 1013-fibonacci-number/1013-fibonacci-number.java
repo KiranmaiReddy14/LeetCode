a61b6f736f8b015cd7fd0fce9a95479481206041
Kiranmai Chigarakula <79890451+KiranmaiReddy14@users.noreply.github.com>
2024-09-29 12:37:51 -0400

    Time: 0 ms (100%), Space: 40.2 MB (66.47%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.java
+++ 1013-fibonacci-number/1013-fibonacci-number.java
@@ -1,8 +1,8 @@
+import java.lang.*;
 class Solution {
     public int fib(int n) {
-        if (n < 2)
-            return n;
-        else
-            return fib(n - 1) + fib(n - 2);
+        double phi = (1 + Math.sqrt(5)) / 2;
+        double psi = (1 - Math.sqrt(5)) / 2;
+        return (int) (Math.round((Math.pow(phi, n) - Math.pow(psi, n)) / Math.sqrt(5)));
     }
 }
