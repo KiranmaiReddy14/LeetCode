60b01e46e1871f23feebac1335b343a425473560
Kiranmai Chigarakula <79890451+KiranmaiReddy14@users.noreply.github.com>
2024-10-03 16:34:17 -0400

    Time: 0 ms (100%), Space: 45.2 MB (43.7%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,20 +1,9 @@
 class Solution {
     public int missingNumber(int[] nums) {
-        int i = 0;
-        int len = nums.length;
-        while (i < len) {
-            int index = nums[i];
-            if (nums[i] < len && i != index) {
-                int temp = nums[i];
-                nums[i] = nums[index];
-                nums[index] = temp;
-            } else
-                i++;
+        int missing = nums.length;
+        for (int i = 0; i < nums.length; i++) {
+            missing ^= i ^ nums[i];
         }
-        for (int j = 0; j < len; j++) {
-            if (j != nums[j])
-                return j;
-        }
-        return len;
+        return missing;
     }
 }
