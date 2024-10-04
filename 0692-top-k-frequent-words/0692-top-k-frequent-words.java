cae8a14f008073f3b580230ba20cf0a7fb15d4f3
Kiranmai Chigarakula <79890451+KiranmaiReddy14@users.noreply.github.com>
2024-10-04 16:06:37 -0400

    Time: 7 ms (65.06%), Space: 44.8 MB (37.21%) - LeetHub

--- 0692-top-k-frequent-words/0692-top-k-frequent-words.java
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.java
@@ -5,23 +5,18 @@ class Solution {
             map.put(s, map.getOrDefault(s, 0) + 1);
 
         PriorityQueue<String> pq = new PriorityQueue<>((a, b) -> {
-            int freqA = map.get(a);
-            int freqB = map.get(b);
-            if (freqA == freqB) {
-                return a.compareTo(b); // Lexicographical order
+            int fa = map.get(a);
+            int fb = map.get(b);
+            if (fa == fb) {
+                return a.compareTo(b);
             }
-            return freqB - freqA; // Higher frequency comes first
+            return fb - fa;
         });
-        
-        // Step 3: Add all words to the priority queue
         pq.addAll(map.keySet());
-        
-        // Step 4: Extract top K frequent words
         List<String> result = new ArrayList<>();
         for (int i = 0; i < k; i++) {
             result.add(pq.poll());
         }
-        
         return result;
     }
 }
