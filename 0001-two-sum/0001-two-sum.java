865ad2c4325b811ec48fa005b60ed61d7e310d00
Kiranmai Chigarakula <79890451+KiranmaiReddy14@users.noreply.github.com>
2024-10-04 12:49:22 -0400

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int i = 0;
+        while (i < nums.length - 1) {
+            if (nums[i] > target)
+                continue;
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target)
+                    return new int[] { i, j };
+            }
+            i++;
+        }
+        return new int[0];
+    }
+}
