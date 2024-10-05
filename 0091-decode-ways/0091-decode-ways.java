5109560f7780ad302c6781e2a0f6e5efc77305b8
Kiranmai Chigarakula <79890451+KiranmaiReddy14@users.noreply.github.com>
2024-10-05 11:51:44 -0400

    Time: 1 ms (80.65%), Space: 41.7 MB (63.7%) - LeetHub

---
+++ 0091-decode-ways/0091-decode-ways.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int numDecodings(String s) {
+        if (s == null || s.length() == 0 || s.charAt(0) == '0')
+            return 0;
+        int len = s.length();
+        int[] dp = new int[len + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+        for (int i = 2; i <= len; ++i) {
+            int one = s.charAt(i - 1) - '0';
+            int two = Integer.parseInt(s.substring(i - 2, i));
+            if (one != 0)
+                dp[i] += dp[i - 1];
+            if (10 <= two && two <= 26)
+                dp[i] += dp[i - 2];
+        }
+        return dp[len];
+    }
+}
