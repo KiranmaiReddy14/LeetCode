d848463ef7844f5375b9fbbc691db4e15d1bd307
Kiranmai Reddy <79890451+KiranmaiReddy14@users.noreply.github.com>
2024-10-27 18:53:07 -0400

    Time: 1 ms (54.15%), Space: 40.5 MB (84.95%) - LeetHub

---
+++ 0258-add-digits/0258-add-digits.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int addDigits(int num) {
+        while (num >= 10) {
+            int temp = num;
+            int sum = 0;
+            while (temp > 0) {
+                int t = temp % 10;
+                temp = temp / 10;
+                sum += t;
+            }
+            num = sum;
+        }
+        return num;
+    }
+}
