8476aed1531ea8b258e9432cc91999c43fc17542
Kiranmai Reddy <79890451+KiranmaiReddy14@users.noreply.github.com>
2024-11-09 16:58:37 -0500

    Time: 2 ms (9.78%), Space: 45.4 MB (97.72%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -4,6 +4,17 @@ class Solution {
         int[] forwardMax = new int[n];
         int[] backwardMax = new int[n];
         int maxHeight = 0;
+
+        // to get the water store we have to calculate what is the max height on the
+        // left
+        // and the right side, along with height we have
+
+        // so by taking two arrays calculating the max height from L to R
+        // and R to L and then subreacting from the min value from both arrays
+
+        // if the value is negative make it 0, other wise add to the array
+        // sum all the water that is trapped
+
         for (int i = 0; i < n; i++) {
             forwardMax[i] = maxHeight;
             maxHeight = Math.max(maxHeight, height[i]);
@@ -19,9 +30,8 @@ class Solution {
             trappedWater[i] = val < 0 ? 0 : val;
         }
         int water = 0;
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++)
             water += trappedWater[i];
-        }
         return water;
     }
 }
