0dbb668bb5bcf5d03abfbcb2ca98f0ab9589196b
Kiranmai Reddy <79890451+KiranmaiReddy14@users.noreply.github.com>
2024-11-09 21:25:27 -0500

    Time: 56 ms (13.18%), Space: 55.1 MB (54.49%) - LeetHub

--- 0940-fruit-into-baskets/0940-fruit-into-baskets.java
+++ 0940-fruit-into-baskets/0940-fruit-into-baskets.java
@@ -5,28 +5,20 @@ class Solution {
         int j = 0;
         int count = 0;
         while (j < fruits.length) {
-            // map.put(fruits[j], map.getOrDefault(fruits[j], 0) + 1);
             map.put(fruits[j], map.getOrDefault(fruits[j], 0) + 1);
-
-            // System.out.println(fruits[j] + " -> " + map.get(fruits[j]) + " " + map.size()
-            // + " " + (j - i));
+            // if the size is > 2, popping the elemnts till the size goes to 2
             while (map.size() > 2) {
                 int c = map.get(fruits[j]);
-                // map.put(fruits[j], 1);
-                // for (Map.Entry<Integer, Integer> m : map.entrySet())
-                // System.out.println(i + " " + fruits[i] + " -> " + map.get(fruits[i]));
-
+                // remove 1 element at a time and increase the
+                // ith element count to get into the right position
                 map.put(fruits[i], map.get(fruits[i]) - 1);
                 if (map.get(fruits[i]) == 0)
                     map.remove(fruits[i]);
-                //System.out.println(map.size() + "   " + j);
-                // i =i+c;
                 i++;
             }
-            count = (j - i)+1 > count ? (j - i)+1 : count;
+            count = (j - i) + 1 > count ? (j - i) + 1 : count;
             j++;
         }
-        // System.out.println(i + " " + j + " " + count+" "+(j-i));
         count = (j - i) > count ? j - i : count;
         return count;
     }
