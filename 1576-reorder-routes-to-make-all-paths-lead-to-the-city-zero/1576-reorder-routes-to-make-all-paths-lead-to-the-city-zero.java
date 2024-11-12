26b9fd9aaa52761e1ece9a0394a2c60e6cd9b072
Kiranmai Reddy <79890451+KiranmaiReddy14@users.noreply.github.com>
2024-11-11 20:30:17 -0500

    Time: 33 ms (90.28%), Space: 73.8 MB (42.71%) - LeetHub

---
+++ 1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int minReorder(int n, int[][] connections) {
+        List<List<Integer>> list = new ArrayList<>();
+        for (int i = 0; i < n; i++)
+            list.add(new ArrayList<>());
+        for (int[] con : connections) {
+            list.get(con[0]).add(con[1]);
+            list.get(con[1]).add(-con[0]);
+        }
+        return DFS(list, 0, n);
+    }
+
+    static int DFS(List<List<Integer>> list, int s, int n) {
+        boolean[] visited = new boolean[n];
+        return DFSRecursion(list, s, visited);
+    }
+
+    static int DFSRecursion(List<List<Integer>> list, int s, boolean[] visited) {
+        int count = 0;
+        visited[s] = true;
+        for (int i : list.get(s)) {
+            if (!visited[Math.abs(i)])
+                count += DFSRecursion(list, Math.abs(i), visited) + (i > 0 ? 1 : 0);
+        }
+        return count;
+    }
+}
