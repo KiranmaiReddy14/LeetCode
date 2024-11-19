68a459bc2f3314d5b031f6255bdf0c6619f2a489
Kiranmai Reddy <79890451+KiranmaiReddy14@users.noreply.github.com>
2024-11-18 22:26:01 -0500

    Time: 0 ms (100%), Space: 40.9 MB (68.61%) - LeetHub

---
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int rob(int[] nums) {
+        int n = nums.length;
+        if (n == 1)
+            return nums[0];
+        return Math.max(getMaxRobbed(nums, 0, n - 2), getMaxRobbed(nums, 1, n - 1));
+    }
+
+    public static int getMaxRobbed(int[] nums, int start, int end) {
+        int prev = 0, max = 0;
+        for (int i = start; i <= end; i++) {
+            int robb = Math.max(max, prev + nums[i]);
+            prev = max;
+            max = robb;
+        }
+        return max;
+    }
+}
