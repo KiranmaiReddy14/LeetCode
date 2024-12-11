f272b3b2036695ba71889eeebad37873131dd24a
Kiranmai Reddy <79890451+KiranmaiReddy14@users.noreply.github.com>
2024-12-11 11:58:43 -0500

    Time: 0 ms (100%), Space: 41.6 MB (15.85%) - LeetHub

--- 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.java
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.java
@@ -1,33 +1,31 @@
 class MyQueue {
-    Stack<Integer> stack;
-    Stack<Integer> temp;
+
+    Stack<Integer> stack1;
+    Stack<Integer> stack2;
 
     public MyQueue() {
-        stack = new Stack<Integer>();
-        temp = new Stack<Integer>();
+        stack1 = new Stack<>();
+        stack2 = new Stack<>();
     }
 
     public void push(int x) {
-        while (!stack.isEmpty()) {
-            temp.push(stack.pop());
-        }
-        stack.push(x);
-        while (!temp.isEmpty()) {
-            stack.push(temp.pop());
-        }
-
+        while (!stack1.isEmpty())
+            stack2.push(stack1.pop());
+        stack1.push(x);
+        while (!stack2.isEmpty())
+            stack1.push(stack2.pop());
     }
 
     public int pop() {
-        return stack.isEmpty() ? 0 : stack.pop();
+        return stack1.isEmpty() ? 0 : stack1.pop();
     }
 
     public int peek() {
-        return stack.isEmpty() ? 0 : stack.peek();
+        return stack1.isEmpty() ? 0 : stack1.peek();
     }
 
     public boolean empty() {
-        return stack.size() == 0 ? true : false;
+        return stack1.isEmpty() ? true : false;
     }
 }
 
