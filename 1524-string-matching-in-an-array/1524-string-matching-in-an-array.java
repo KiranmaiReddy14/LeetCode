48a7d505f40a977ac48948ef5b6286b8e59eafbc
Kiranmai Reddy <79890451+KiranmaiReddy14@users.noreply.github.com>
2025-01-05 21:17:07 -0500

    Time: 4 ms (86.3%), Space: 42.3 MB (53.85%) - LeetHub

--- 1524-string-matching-in-an-array/1524-string-matching-in-an-array.java
+++ 1524-string-matching-in-an-array/1524-string-matching-in-an-array.java
@@ -3,9 +3,8 @@ class Solution {
         List<String> list = new ArrayList<>();
         for (int i = 0; i < words.length; i++) {
             for (int j = 0; j < words.length; j++) {
-                if (i != j && words[i].contains(words[j]) && !list.contains(words[j])) {
+                if (i != j && words[i].contains(words[j]) && !list.contains(words[j]))
                     list.add(words[j]);
-                }
             }
         }
         return list;
