fb7a07f792706fec452506424b6a2741b79bda0d
Kiranmai Reddy <79890451+KiranmaiReddy14@users.noreply.github.com>
2025-01-21 13:14:00 -0500

    Time: 13 ms (74.35%), Space: 48.7 MB (51.1%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,22 +1,18 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
         HashMap<Integer, Integer> map = new HashMap<>();
-        for (int n : nums) {
+        for(int n : nums)
             map.put(n, map.getOrDefault(n, 0) + 1);
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> b[0]-a[0]);
+        for(Map.Entry<Integer, Integer> m : map.entrySet())
+            pq.add(new int[]{m.getValue(), m.getKey()});
+        int[] res=new int[k];
+        int i = 0;
+        while(k > 0) {
+            res[i] = pq.poll()[1];
+            i++;
+            k--;
         }
-        List<Integer>[] list = new List[nums.length + 1];
-        for (Map.Entry<Integer, Integer> m : map.entrySet()) {
-            int fre = m.getValue();
-            if (list[fre] == null)
-                list[fre] = new ArrayList<>();
-            list[fre].add(m.getKey());
-        }
-        List<Integer> li = new ArrayList<>();
-        for (int j = list.length - 1; j >= 0 && li.size() < k; j--) {
-            if (list[j] != null) {
-                li.addAll(list[j]);
-            }
-        }
-        return li.stream().mapToInt(i -> i).toArray();
+        return res;
     }
 }
