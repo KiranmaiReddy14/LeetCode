1c68c7abe354a0d63c3fbd5dab40edc74ec4ea1d
Kiranmai Reddy <79890451+KiranmaiReddy14@users.noreply.github.com>
2025-02-03 11:12:07 -0500

    Time: 1 ms (93.94%), Space: 42.7 MB (73.06%) - LeetHub

--- 3372-longest-strictly-increasing-or-strictly-decreasing-subarray/3372-longest-strictly-increasing-or-strictly-decreasing-subarray.java
+++ 3372-longest-strictly-increasing-or-strictly-decreasing-subarray/3372-longest-strictly-increasing-or-strictly-decreasing-subarray.java
@@ -4,27 +4,24 @@ class Solution {
         if (n < 2)
             return n;
         int max = 1;
-        int count = 1;
-        for (int i = 0; i < n - 1; i++) {
-            if (nums[i + 1] > nums[i]) {
-                count++;
-                continue;
+        int inc = 1, dec = 1;
+        for (int i = 1; i < n; i++) {
+            // if it's in increasing order increment inc variable
+            // as it's incrementing make dec as 1
+            if (nums[i] > nums[i - 1]) {
+                inc++;
+                dec = 1;
+            } else if (nums[i] < nums[i - 1]) {
+                dec++;
+                inc = 1;
             }
-            max = Math.max(max, count);
-            count = 1;
-        }
-        max = Math.max(max, count);
-        count = 1;
-        // for decreased nums
-        for (int i = 0; i < n - 1; i++) {
-            if (nums[i] > nums[i + 1]) {
-                count++;
-                continue;
+            // if the values are same reset both the variables
+            else {
+                inc = 1;
+                dec = 1;
             }
-            max = Math.max(max, count);
-            count = 1;
+            max = Math.max(inc, Math.max(max, dec));
         }
-        max = Math.max(max, count);
         return max;
     }
 }
